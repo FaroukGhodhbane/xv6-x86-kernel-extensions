311772eeb05b9aa1389f240a87c3e0a06ba23f16
Farouk Ghodhbane <fghodbane99@gmail.com>
2026-07-28 18:34:34 -0700

    test(syscall): complete getLCADistance test suite

--- Makefile
+++ Makefile
@@ -78,7 +78,7 @@ AS = $(TOOLPREFIX)gas
 LD = $(TOOLPREFIX)ld
 OBJCOPY = $(TOOLPREFIX)objcopy
 OBJDUMP = $(TOOLPREFIX)objdump
-CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -O2 -Wall -MD -ggdb -m32 -Werror -fno-omit-frame-pointer -Wno-array-bounds -Wno-infinite-recursion -Wno-unused-but-set-variable
+CFLAGS = -fno-pic -static -fno-builtin -fno-strict-aliasing -O2 -Wall -MD -ggdb -m32 -Werror -fno-omit-frame-pointer -Wno-array-bounds -Wno-infinite-recursion -Wno-unused-but-set-variable -Wno-unused-variable
 CFLAGS += $(shell $(CC) -fno-stack-protector -E -x c /dev/null >/dev/null 2>&1 && echo -fno-stack-protector)
 ASFLAGS = -m32 -gdwarf-2 -Wa,-divide
 # FreeBSD ld wants ``elf_i386_fbsd''
@@ -183,6 +183,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_test_getlcadistance\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
--- test_getlcadistance.c
+++ test_getlcadistance.c
@@ -3,14 +3,14 @@
 #include "user.h"
 
 // Case 0 runs all tests
-void test_invalid_pids(void) {}  // case 1
-void test_init(void) {}  // case 2
-void test_self(void) {}  // case 3
-void test_child_parent(void) {}  // case 4
-void test_siblings(void) {}  // case 5
-void test_grandchild_grandparent(void) {}  // case 6
+void test_invalid_pids(void);  // case 1
+void test_init(void); // case 2
+void test_self(void);  // case 3
+void test_child_parent(void);  // case 4
+void test_siblings(void);  // case 5
+void test_grandchild_grandparent(void);  // case 6
 
-// Helper function for printing
+// Helper function for printing results
 void assert_distance(
   int pid1,
   int pid2,
@@ -72,4 +72,86 @@ int main(int argc, char* argv[]){
 
   printf(1, "--- Test Suite Completed ---\n");
   exit();
+}
+
+// case 1
+void test_invalid_pids(void){
+  int test_pid = getpid();
+  assert_distance(100, test_pid, -2, "Invalid pid1");
+  assert_distance(test_pid, 100, -2, "Invalid pid 2");
+}
+ 
+// case 2
+void test_init(void){  // case 2
+  int test_pid = getpid();
+  assert_distance(test_pid, 1, -1, "Boundary Check Against init (PID = 1)");
+  assert_distance(1, test_pid, -1, "Boundary Check Against init (PID = 1)");
+}
+
+// case 3
+void test_self(void){
+  int test_pid = getpid();
+  assert_distance(test_pid, test_pid, 0, "Same Process Check");
+}
+
+// case 4
+void test_child_parent(void){
+  int parent_process_pid = getpid();
+  int child_process_pid = fork();
+  if(child_process_pid == 0){
+    sleep(100);  // Let child sleep long enough for the assertion
+    exit();
+  }
+
+  assert_distance(parent_process_pid, child_process_pid, 1, "Parent to Child Check");
+  assert_distance(child_process_pid, parent_process_pid, 1, "Child to Parent Check");
+
+  kill(child_process_pid);  // Terminate child process
+  wait();  // Clean process table
+}
+
+// case 5
+void test_siblings(void){
+  int child_process_1_pid = fork();
+  if(child_process_1_pid == 0){
+    sleep(100);
+    exit();
+  }
+
+  int child_process_2_pid = fork();
+  if(child_process_2_pid == 0){
+    sleep(100);
+    exit();
+  }
+
+  assert_distance(child_process_1_pid, child_process_2_pid, 2, "Siblings One Direction Check");
+  assert_distance(child_process_2_pid, child_process_1_pid, 2, "Siblings Other Direction Check");
+
+  kill(child_process_1_pid);
+  kill(child_process_2_pid);
+  wait();
+  wait();
+}
+
+// case 6
+void test_grandchild_grandparent(void){
+  int grandparent_process_pid = getpid();
+  int child_process_pid = fork();
+
+  if(child_process_pid == 0){
+    int grandchild_process_pid = fork();
+
+    if(grandchild_process_pid == 0){
+      sleep(100);
+      exit();
+    }
+
+    assert_distance(grandparent_process_pid, grandchild_process_pid, 2, "Grandparent to Grandchild Check");
+    assert_distance(grandchild_process_pid, grandparent_process_pid, 2, "Grandchild to Grandparent Check");
+
+    kill(grandchild_process_pid);
+    wait();
+    exit();
+  }
+  wait();
 }
